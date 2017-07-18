7def253af48860260dfdfe42c706aa7f61fb14ae
Dereck McDuff <derrmcduff@me.com>
2017-07-18 03:06:56 -0400

    IT'S HUGE

--- mymalloc.c
+++ mymalloc.c
@@ -39,7 +39,7 @@ void new_bloc(void* addr) {
   
   newBloc.occ = 1;
   newBloc.nxtLinked = 0;
-  newBloc.start = mmap(addr, 4096,  PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+  newBloc.start = mmap(addr, 4096, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
   newBloc.end = newBloc.start + 4096;
   newBloc.size = 4096 - (sizeof(size_t) + sizeof(bloc));
   newBloc.prv = lastBloc;
@@ -121,7 +121,7 @@ void myfree(void *ptr){
   region *focusedRegion = firstBloc.start;
   int found = 0;
   
-  while ((found == 0) && (focusedRegion.occ == 1)) {
+  while ((found == 0) && (focusedRegion.occ == 1) && (focusedRegion != NULL)) {
     if &focusedRegion == &ptr {
       found == 1;
       break;
@@ -130,25 +130,41 @@ void myfree(void *ptr){
   }
   
   if (found == 1) {
-    killRegion(&focusedRegion);
+    killRegion(&focusedRegion, 0);
   } else {
-    
+    printf("this thing was ")
   }
   
 }
 
-int killRegion(region *dyingRgn) {
+int killRegion(region *dyingRgn, int direction) {
   
-  region *prevRgn = dyingRgn.prev
-  if prevRgn != NULL {
-    
-  } 
+  region *prevRgn = dyingRgn.prev;
+  if prevRgn != NULL && (prevRgn.occ == 0) && ((direction == 0) || (direction == -1)) {
+    killRegion(&prevRgn, -1);
+    mergeRegions(&prevRgn, &dyingRgn);
+  }
   
   dyingRgn.occ = 0;
   munmap(dyingRgn.start, dyingRgn.size);
+  
+  
+  region *nextRgn = nextRgn.next
+  if nextRgn != NULL && (prevRgn.occ == 0) && ((direction == 0) || (direction == 1)) {
+    killRegion(&nextRgn, 1);
+    mergeRegions(&dyingRgn, &nextRgn);
+  }
+  munmap(&dyingRgn, (dyingRgn.size + sizeof(dyingRgn) + sizeof(size_t)));
 }
 
-int killRegion()
+int mergeRegions(region *rgn1, region *rgn2) {
+  int sizeToExpandRgn1 = sizeof(size_t) + sizeof(rgn2) + rgn2.size;
+  rgn1.size = rgn1.size + sizeToExpandRgn1;
+  rgn1.end = rgn2.end;
+  rgn1.next = rgn2.next;
+  region *nextRgn = rgn1.next;
+  nextRgn.prev = &rgn1;
+}
 
 int roundUp(size_t size) 
 {
